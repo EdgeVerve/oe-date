cc065ffcf9a29ca4602bb8e499260839fbdd15f6
ujwala <sirigineedi.ujwala@infosys.com>
2019-02-18 15:41:10 +0530

    Moving to polymer 3

---
+++ oe-datepicker-dlg.js
@@ -0,0 +1,315 @@
+/**
+ * @license
+ * ©2018-2019 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
+ * Bangalore, India. All Rights Reserved.
+ */
+
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { LegacyElementMixin } from "@polymer/polymer/lib/legacy/legacy-element-mixin.js";
+import "@polymer/paper-dialog/paper-dialog.js";
+import "@polymer/paper-button/paper-button.js";
+import "@polymer/iron-flex-layout/iron-flex-layout.js";
+import "./oe-datepicker.js";
+import "oe-i18n-msg/oe-i18n-msg.js";
+
+/**
+ * # oe-datepicker-dlg
+ * 
+ * `<oe-datepicker-dlg>` is a control used in oe-date to display the date-picker in a dialog box.
+ * Raises event `oe-date-picked` when date is selected.
+ * 
+ * @customElement
+ * @polymer
+ * 
+ */
+class OeDatepickerDlg extends LegacyElementMixin(PolymerElement) {
+  static get is() { return 'oe-datepicker-dlg'; }
+
+  static get template() {
+    return html`
+      <style>
+        :host {
+          --dpg-header-text: var(--primary-background-color);
+          --dpg-default-background: var(--primary-background-color);
+          --dpg-header-background: var(--default-primary-color);
+          --dpg-dark-background: var(--dark-primary-color);
+        }
+  
+        #dialog div.container {
+          margin: 0;
+          padding: 0;
+          background-color: var(--dpg-default-background);
+        }
+  
+        #dialog:host > ::content > *:first-child {
+          margin-top: 0;
+        }
+  
+        .vertical {
+          @apply --layout-vertical;
+        }
+  
+        .filler {
+          @apply --layout-flex;
+        }
+  
+        .horizontal {
+          @apply --layout-horizontal;
+        }
+  
+        .header {
+          background-color: var(--dpg-header-background);
+          color: var(--dpg-header-text);
+  
+          @apply --layout-vertical;
+          @apply --layout-center;
+        }
+  
+        .dayOfWeekContainer {
+          width: 100%;
+          padding-top: 8px;
+          padding-bottom: 8px;
+          background-color: var(--dpg-dark-background);
+          text-align: center;
+          font-size: 18px;
+          min-height: 18px;
+          font-weight: 300;
+        }
+  
+        .dayOfMonthContainer {
+          padding-left: 10px;
+          padding-right: 10px;
+          font-size: 89px;
+          min-height: 89px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          cursor: pointer;
+        }
+  
+        .monthContainer,
+        .yearContainer {
+          text-transform: none;
+          padding: 0;
+          font-size: 24px;
+          align-items: center;
+          justify-content: center;
+          min-height: 24px;
+        }
+  
+        .monthContainer {
+          padding-top: 10px;
+          font-weight: 300;
+        }
+  
+        .yearContainer {
+          font-weight: 400;
+        }
+  
+        @media (max-width: 600px) {
+          .header {
+            display: none !important;
+          }
+          #dialog {
+            min-width: 300px;
+          };
+        }
+  
+        @media (min-width: 601px) {
+          
+            #dialog {
+              min-width: 435px;
+            };
+        }
+      </style>
+      <paper-dialog id="dialog" on-iron-overlay-opened="patchOverlay">
+        <div class="container layout horizontal no-padding">
+          <div class="header layout vertical">
+            <div class="dayOfWeekContainer" id="day"><span>{{day}}</span></div>
+            <div class="dayOfMonthContainer flex" id="date" on-tap="_showMonth">
+              <paper-ripple></paper-ripple>
+              <span>{{date}}</span>
+            </div>
+            <div class="layout vertical flex">
+              <paper-button class="monthContainer" id="month" on-tap="_showYear">{{month}}</paper-button>
+              <paper-button class="yearContainer" id="year" on-tap="_showDecade">{{year}}</paper-button>
+            </div>
+          </div>
+          <div class="layout flex">
+            <div class="layout vertical flex">
+              <oe-datepicker id="datePicker" value="{{localValue}}" locale="[[locale]]" start-of-week="[[startOfWeek]]" disabled-days="[[disabledDays]]" holidays="[[holidays]]" 
+                max=[[max]] min=[[min]]></oe-datepicker>
+              <div class="layout horizontal">
+                <div class="filler"></div>
+                <paper-button id="cancelBtn" on-tap="_onCancel">
+                    <oe-i18n-msg msgid="cancel"></oe-i18n-msg>
+                </paper-button>
+                <paper-button id="okBtn" on-tap="_onOK">
+                    <oe-i18n-msg msgid="ok"></oe-i18n-msg>
+                </paper-button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </paper-dialog>
+    `;
+  }
+
+  static get properties() {
+    return {
+
+      value: {
+        type: Date,
+        notify: true
+      },
+
+      /**
+       * Denotes the day to be used as the start of week
+       */
+      startOfWeek: {
+        type: Number,
+        value: 1
+      },
+
+      locale: {
+        type: String,
+        value: navigator.language
+      },
+
+      /**
+       * Array of days of the week to set as disabled.
+       * Example [5,6] to set 5th and 6th day of week as disabled.
+       */
+      disabledDays: {
+        type: Array
+      },
+      /**
+       *  List of dates that should be disabled for selection,
+       *  Should be Date objects or date strings in 'MM-DD-YYYY' format
+       */
+      holidays: {
+        type: Array
+      },
+      /**
+       * Maximum selectable date
+       */
+      max: Date,
+
+      /**
+       * Minimum selectable date
+       */
+      min: Date
+
+      /**
+       * Occurs when a date is selected by pressing the Ok button.
+       *
+       * @event oe-date-picked
+       */
+    };
+  }
+
+  /**
+   * Opens the dialog box by setting the value aor current Date in UTC
+   */
+  open() {
+    if (this.value) {
+      this.set('localValue', this.value);
+    } else {
+      var now = new Date();
+      if (now > this.max) {
+        now = this.max;
+      } else if (now < this.min) {
+        now = this.min;
+      }
+      this.set('localValue', new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate())));
+    }
+    this._refreshDetails({
+      detail: this.$.datePicker.getDetails(this.localValue)
+    });
+    this.$.dialog.open();
+  }
+
+  /**
+   * Displays all the days of the selected month
+   */
+  _showMonth() {
+    this.$.datePicker.set('showing', 'month');
+  }
+
+  /**
+   * Displays all the months of the selected year
+   */
+  _showYear() {
+    this.$.datePicker._showYear();
+  }
+
+  /**
+   * Displays all the years of the selected decade.
+   */
+  _showDecade() {
+    this.$.datePicker._showDecade();
+  }
+
+  /**
+   * Refreshes the display with date from the event.
+   * @param {Event} e 
+   */
+  _refreshDetails(e) {
+    this.set('day', e.detail.day);
+    this.set('date', new Intl.NumberFormat(this.locale, {
+      minimumIntegerDigits: 2
+    }).format(e.detail.date));
+    this.set('month', e.detail.month);
+    this.set('year', e.detail.year);
+  }
+
+  /**
+   * Listener to double-click of a date in datepicker.
+   * Sets day,month,year and 'value'.
+   * @param {Event} e 
+   */
+  _selectionConfirmed(e) {
+    this._refreshDetails(e);
+    this._onOK();
+  }
+
+  /**
+   * Sets the selected value and closes the dialog.
+   */
+  _onOK() {
+    this.set('value', this.localValue);
+    this.fire('oe-date-picked', this.value);
+    this.$.dialog.close();
+  }
+
+  /**
+   * Closes the dialog box
+   */
+  _onCancel() {
+    this.set('localValue', this.value);
+    this.$.dialog.close();
+  }
+
+  /**
+   * Pactch to move the backdrop behind the dialog box.
+   * @param {Event} e 
+   */
+  patchOverlay(e) {
+    if (e.target.withBackdrop) {
+      e.target.parentNode.insertBefore(e.target._backdrop, e.target);
+    }
+  }
+
+
+  /**
+   * Connected Callback to initiate event listeners.
+   */
+  connectedCallback() {
+    super.connectedCallback();
+    this.$.datePicker.addEventListener('selection-changed', e => this._refreshDetails(e));
+    this.$.datePicker.addEventListener('selection-double-click', e => this._selectionConfirmed(e));
+  }
+
+}
+
+window.customElements.define(OeDatepickerDlg.is, OeDatepickerDlg);
---
+++ oe-time.js
@@ -0,0 +1,213 @@
+/**
+ * @license
+ * ©2018-2019 EdgeVerve Systems Limited (a fully owned Infosys subsidiary),
+ * Bangalore, India. All Rights Reserved.
+ */
+
+import { html, PolymerElement } from "@polymer/polymer/polymer-element.js";
+import { mixinBehaviors } from "@polymer/polymer/lib/legacy/class.js";
+import { IronValidatableBehavior } from "@polymer/iron-validatable-behavior/iron-validatable-behavior.js";
+import { OETimeMixin } from "oe-mixins/oe-time-mixin.js";
+import "@polymer/iron-input/iron-input.js";
+import "@polymer/iron-flex-layout/iron-flex-layout.js";
+import "@polymer/paper-input/paper-input-container.js";
+import "@polymer/paper-styles/default-theme.js";
+import "@polymer/paper-styles/typography.js";
+import "@polymer/paper-button/paper-button.js";
+
+
+/**
+ * # oe-time
+ * 
+ * `<oe-time>` is a control  to capture `time` data.
+ * 
+ * @customElement
+ * @polymer
+ * @appliesMixin OETimeMixin
+ */
+class OeTime extends mixinBehaviors([IronValidatableBehavior], PolymerElement) {
+    static get is() { return 'oe-time'; }
+
+    static get template() {
+        return html`
+        <style>
+                span {
+                    text-align: center;
+                    padding-top: 2px;
+                    @apply --paper-input-container-font;
+                }
+
+                input[is="iron-input"] {
+                    position: relative;
+                    /* to make a stacking context */
+                    outline: none;
+                    box-shadow: none;
+                    padding: 0;
+                    width: 100%;
+                    background: transparent;
+                    border: none;
+                    color: var(--paper-input-container-input-color, --primary-text-color);
+                    text-align: center;
+
+                    @apply --layout-flex;
+                    @apply --paper-font-subhead;
+                    @apply --paper-input-container-input;
+                }
+
+                .container {
+                    @apply --layout-horizontal;
+                }
+
+                .date-button {
+                    color: var(--secondary-text-color);
+                    padding: 0;
+                    margin: 0;
+                    min-width: 0;
+                }
+
+        </style>
+        <span aria-hidden id="hourLabel" hidden>Hour</span>
+        <span aria-hidden id="minuteLabel" hidden>Minute</span>
+        <paper-input-container id="container" no-label-float="[[noLabelFloat]]" always-float-label="[[alwaysFloatLabel]]" disabled$="[[disabled]]"
+          invalid="[[invalid]]">
+          <div slot="input" class="container">
+            <iron-input  id="hour" invalid="{{invalid}}"  bind-value="{{txtHour}}" allowed-pattern="[0-9]">
+                <input aria-labelledby$="[[_computeAriaLabel(ariaLabelPrefix,'hourLabel')]]" required$="[[required]]"
+                maxlength="2" value="{{txtHour}}" placeholder="HH" prevent-invalid-input autocomplete="cc-exp-month"
+                type="tel" disabled$="[[disabled]]" autofocus$="[[autofocus]]" inputmode$="[[inputmode]]" readonly$="[[readonly]]">
+            </iron-input>
+            <span><b>:</b></span>
+            <iron-input id="minute" invalid="{{invalid}}"  bind-value="{{txtMinute}}" allowed-pattern="[0-9]">
+                <input aria-labelledby$="[[_computeAriaLabel(ariaLabelPrefix,'minuteLabel')]]" required$="[[required]]"
+                maxlength="2" value="{{txtMinute}}" placeholder="MM" prevent-invalid-input autocomplete="cc-exp-year"
+                type="tel" disabled$="[[disabled]]" inputmode$="[[inputmode]]" readonly$="[[readonly]]">
+            </iron-input>
+          </div>
+          <paper-button class="date-button" slot="suffix" on-tap="_toggleAMPM">{{txtAMPM}}</paper-button>
+        </paper-input-container>
+    `;
+    }
+
+    static get properties() {
+        return {
+
+            /**
+             * Set to true to mark the input as required.
+             */
+            required: {
+                type: Boolean,
+                value: false
+            },
+
+            /**
+             * The hour component of the time displayed.
+             */
+            hour: {
+                type: Number,
+                notify: true
+            },
+
+            /**
+             * The minute component of the time displayed.
+             */
+            minute: {
+                type: Number,
+                notify: true
+            },
+
+            txtAMPM: {
+                type: String,
+                value: 'AM'
+            },
+
+            ariaLabelPrefix: {
+                type: String
+            },
+
+            /**
+             * Set to true to disable the hour and minute input elements.
+             */
+            disabled: {
+                type: Boolean,
+                value: false
+            },
+
+            /**
+             * Set to true to autofocus the month input element.
+             */
+            autofocus: {
+                type: Boolean
+            },
+
+            /**
+             * Bound to the hour and minute input elements' `inputmode` property.
+             */
+            inputmode: {
+                type: String
+            },
+
+            /**
+             * Set to true to mark the hour and minute inputs as not editable.
+             */
+            readonly: {
+                type: Boolean,
+                value: false
+            },
+
+            txtHour: {
+                type: String
+            },
+
+            txtMinute: {
+                type: String
+            }
+        };
+    }
+
+    /**
+    * Connected Callback to initiate event listeners.
+    */
+    connectedCallback() {
+        super.connectedCallback();
+        this.$.hour.dataMax = 12;
+        this.$.hour.dataMin = 1;
+        this.$.minute.dataMax = 59;
+        this.$.minute.dataMin = 0;
+    }
+
+
+    /**
+     * Sets the AM/PM and pads the hour and minute values.
+     * @param {number} newHour 
+     * @param {number} newMinute 
+     */
+    _valuesChanged(newHour, newMinute) {
+        this.$.hour.value = this._hoursDisplay(newHour);
+        this.$.minute.value = this._minutesDisplay(newMinute);
+    }
+
+
+    /**
+     * Custom validation
+     */
+    validate() {
+        // Empty, non-required input is valid.
+        if (!this.required && this.minute && this.hour == '') {
+            return true;
+        }
+        return !this.invalid;
+    }
+
+    /**
+     * Computes aria-label
+     * @param {string} hourLabel 
+     * @param {string} minuteLabel 
+     * @return {string}
+     */
+    _computeAriaLabel(hourLabel, minuteLabel) {
+        return hourLabel + ' ' + minuteLabel;
+    }
+
+}
+
+window.customElements.define(OeTime.is, OETimeMixin(OeTime));
